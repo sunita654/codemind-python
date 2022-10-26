9e1368b10d92e952a6cfc5f56797e59638ff9cb7
sunita654 <94676011+sunita654@users.noreply.github.com>
2022-10-26 22:43:21 +0530

    hf,sf,sfr=map(int,input().split(" ")) if(hf>50 and sf>60 and sfr>100):print("10") elif(hf>50 and sf>60):print("9") elif(sf>60 and sfr>100):print("8") elif(hf>50 and sfr>100):print("7") elif(hf>50 or sf>60 or sfr>100):print("6") else:print("5")

---
+++ Grades_of_Rides.py
@@ -0,0 +1,7 @@
+hf,sf,sfr=map(int,input().split(" "))
+if(hf>50 and sf>60 and sfr>100):print("10")
+elif(hf>50 and sf>60):print("9")
+elif(sf>60 and sfr>100):print("8")
+elif(hf>50 and sfr>100):print("7")
+elif(hf>50 or sf>60 or sfr>100):print("6")
+else:print("5")
