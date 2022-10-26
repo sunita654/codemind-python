e95f569bec0e9c8ebad4f52c1fcb07c427886faf
sunita654 <94676011+sunita654@users.noreply.github.com>
2022-10-26 21:58:17 +0530

    h,m=map(int,input().split(":")) if (h == 12):        h = 0 if (m == 60):     m = 0     h += 1     if(h>12):         h = h-12 hour_angle = (0.5 * (h*60 + m)) minute_angle = (6*m) angle = abs(hour_angle - minute_angle) angle = min(360-angle, angle) print(angle)

---
+++ Angle_Between_Hands.py
@@ -0,0 +1,13 @@
+h,m=map(int,input().split(":"))
+if (h == 12):
+       h = 0
+if (m == 60):
+    m = 0
+    h += 1
+    if(h>12):
+        h = h-12
+hour_angle = (0.5 * (h*60 + m))
+minute_angle = (6*m)
+angle = abs(hour_angle - minute_angle)
+angle = min(360-angle, angle)
+print(angle)
