04095032c4ad1a68e90e2f998522749118427dbf
sunita654 <94676011+sunita654@users.noreply.github.com>
2022-11-02 22:55:25 +0530

    def areBracketsBalanced(expr):     stack = []     for char in expr:         if char in ["(", "{", "["]:             stack.append(char)         else:             if not stack:                 return False             current_char = stack.pop()             if current_char == '(':                 if char != ")":                     return False             if current_char == '{':                 if char != "}":                     return False             if current_char == '[':                 if char != "]":                     return False
        if stack: return False
        return True
    e=input()
    print(areBracketsBalanced(e))

---
+++ Balanced_Parentheses_1.py
@@ -0,0 +1,23 @@
+def areBracketsBalanced(expr):
+    stack = []
+    for char in expr:
+        if char in ["(", "{", "["]:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            current_char = stack.pop()
+            if current_char == '(':
+                if char != ")":
+                    return False
+            if current_char == '{':
+                if char != "}":
+                    return False
+            if current_char == '[':
+                if char != "]":
+                    return False
+                    
+    if stack: return False
+    return True
+e=input()
+print(areBracketsBalanced(e))
