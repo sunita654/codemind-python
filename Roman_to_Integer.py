0377c6664ce4fa8bdd9b13c5659fe291e1e04bc2
sunita654 <94676011+sunita654@users.noreply.github.com>
2022-11-02 23:37:26 +0530

    class Solution(object):    def romanToInt(self, s):       roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}       i = 0       num = 0       while i < len(s):          if i+1<len(s) and s[i:i+2] in roman:             num+=roman[s[i:i+2]]             i+=2          else:             #print(i)             num+=roman[s[i]]             i+=1       return num ob1 = Solution() print(ob1.romanToInt(input()))

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,12 +1,16 @@
-class py_solution:
-    def roman_to_int(self, s):
-        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        int_val = 0
-        for i in range(len(s)):
-            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
-                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
-            else:
-                int_val += rom_val[s[i]]
-        return int_val
-
-print(py_solution().roman_to_int(input()))
+class Solution(object):
+   def romanToInt(self, s):
+      roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+      i = 0
+      num = 0
+      while i < len(s):
+         if i+1<len(s) and s[i:i+2] in roman:
+            num+=roman[s[i:i+2]]
+            i+=2
+         else:
+            #print(i)
+            num+=roman[s[i]]
+            i+=1
+      return num
+ob1 = Solution()
+print(ob1.romanToInt(input()))
