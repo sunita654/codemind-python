9ae7b2055a6073b7dba466eabc7dbbd847fcb7de
sunita654 <94676011+sunita654@users.noreply.github.com>
2022-11-08 22:47:56 +0530

    def unique(n):     for i in n:         if n.count(i)>1:return "Not Unique Number"     return "Unique Number" n=input() print(unique(n))

--- Unique_number.py
+++ Unique_number.py
@@ -1,6 +1,6 @@
+def unique(n):
+    for i in n:
+        if n.count(i)>1:return "Not Unique Number"
+    return "Unique Number"
 n=input()
-l=[]
-for i in n:
-    if i not in l:l.append(i)
-if(len(l)==len(n)):print("Unique Number")
-else:print("Not Unique Number")
+print(unique(n))
