470427eec897cc7cd05de1026c014f2429adba42
sunita654 <94676011+sunita654@users.noreply.github.com>
2022-11-08 23:18:21 +0530

    def isprime(num):     flag = False     if num > 1:         for i in range(2, num):              if (num % i) == 0:                  flag = True                  break     return flag n=int(input()) k=str(n) if(isprime(n)):print("not prime") else:     if(isprime(int(k[::-1]))):         print("prime but not a circular prime")     else:         print("circular prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,15 +1,16 @@
-def isPrime(num):
+def isprime(num):
     flag = False
     if num > 1:
         for i in range(2, num):
-              if (num % i) == 0:
-                     flag = True
-                     break
+             if (num % i) == 0:
+                 flag = True
+                 break
     return flag
 n=int(input())
 k=str(n)
-if isPrime(n):
-     print("not prime")
+if(isprime(n)):print("not prime")
 else:
-    if(isPrime(int(k[::-1]))):print("prime but not a circular prime")
-    else:print("circular prime")
+    if(isprime(int(k[::-1]))):
+        print("prime but not a circular prime")
+    else:
+        print("circular prime")
